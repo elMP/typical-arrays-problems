1820165e0cfd1ae537d00ed517c75fe0024fb326
kneazle <kneazle@yandex.ru>
2021-08-22 01:40:06 +0300

    feat: add simple stupid solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length === 0)
+    return 0;
+  let min = array[0];
+  array.forEach(item => {
+    if (item < min)
+      min = item;
+  })
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length === 0)
+    return 0;
+  let max = array[0];
+  array.forEach(item => {
+    if (item > max)
+      max = item;
+  })
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length === 0)
+    return 0;
+  let sum = 0;
+  array.forEach(item => {
+    sum += item;
+  })
+  return sum / array.length;
 }
